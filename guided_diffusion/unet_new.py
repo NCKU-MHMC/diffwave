2bf23eea7c742a50f2647c1ec239998bdee5cb42
toonnyy8 <toonnyy8@gmail.com>
2025-05-27 15:30:02 +0800

    fix w2v2 prerpocess

--- guided_diffusion/unet_new.py
+++ guided_diffusion/unet_new.py
@@ -723,7 +723,8 @@ class Speech2Vector(nn.Module):
         if mask is None:
             x = th.vmap(lambda v: (v - v.mean())/v.std().clamp_min(1e-6))(x)  # normalize each sample
         else:
-            n = (~mask).float().sum(1, keepdim=True).clamp_min(1e-6)
+            n = (~mask).float().sum(1, keepdim=True)
+            n[n==0] = x.shape[1]
             x_mu = (x * (~mask).float()).sum(1, keepdim=True)/n  # mean of non-masked values
             x_std = ((x * (~mask).float()).pow(2).sum(1, keepdim=True)/n - x_mu.pow(2)).sqrt()
             x = ((x - x_mu) / x_std.clamp_min(1e-6)) * (~mask).float()  
