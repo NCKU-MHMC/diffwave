ddd974f2b048854a821aaf4cc6ad77dd2d66fb71
toonnyy8 <toonnyy8@gmail.com>
2025-05-30 19:42:33 +0800

    v6.1

--- train_v6.sh
+++ train_v6.sh
@@ -1,9 +1,9 @@
 # Run generate_images.py using Docker
-docker run --gpus all -it --rm --user $(id -u):$(id -g) \
+docker run --gpus all -it --rm \
     -v `pwd`:/scratch --workdir /scratch -e HOME=/scratch \
     -v /media/8tsp/dataset:/datasets \
     -e HYDRA_FULL_ERROR=1 \
-    -e OPENAI_LOGDIR="./.experiments/libritts-v6" \
+    -e OPENAI_LOGDIR="./.experiments/libritts-v6.1" \
     -e OPENAI_LOG_FORMAT="stdout,csv,tensorboard" \
     --shm-size 128G \
     diffwave:latest \
